b8576fb520086e9f0839341a72034df66d745d8d
rwinklerwilkes <rich.w.sas@gmail.com>
2024-12-28 08:43:27 -0500

    Dealing with tuple issues

--- day18.py
+++ day18.py
@@ -1,5 +1,6 @@
 from aocd import get_data
 from collections import defaultdict
+import heapq as hq
 import numpy as np
 
 data = get_data(day=18,year=2024)
@@ -58,37 +59,41 @@ def print_grid(grid, t, mxh, mxw):
 def tadd(t1, t2):
     return (t1[0]+t2[0], t1[1]+t2[1])
 
-def get_neighbors(grid, pos):
+def get_neighbors(grid, pos, t):
     neighbor_dict = {'up': (-1, 0),
                      'left': (0, -1),
                      'down': (1, 0),
                      'right': (0, 1)}
     neighbors = {cdir: tadd(pos, v) for cdir, v in neighbor_dict.items()}
-    neighbors = {cdir: n for cdir,n in neighbors.items() if grid[n] != '#'}
+    grid_at_time = {(k[1],k[2]):v for k,v in grid.items() if k[0] < t}
+    neighbors = {cdir: n for cdir, n in neighbors.items() if grid_at_time.get(n,'.') != '#'}
     return neighbors
 
-def dijkstra(grid, mxw, mxh):
+def dijkstra(grid, mxw, mxh, t):
     start = (0,0)
-    end = ()
-    dist = {}
+    end = (mxw, mxh)
+    dist = defaultdict(int)
     for k,_ in grid.items():
-        dist[k] = np.inf
+        if k[0] < t:
+            dist[(k[1],k[2])] = np.inf
     prev = defaultdict(tuple)
     q = []
-    hq.heappush(q, (0,pos,'right'))
-    dist[pos] = 0
+    hq.heappush(q, (0,start))
+    dist[start] = 0
 
     while q:
         cnode = hq.heappop(q)
-        p, u, current_direction = cnode
-        all_neighbors = get_neighbors(grid, u)
+        p, u = cnode
+        all_neighbors = get_neighbors(grid, u, t)
         for ndir, n in all_neighbors.items():
-            alt = dist[u] + rotate(current_direction, ndir) * 1000 + 1
+            alt = dist[u] + 1
             if alt < dist[n]:
                 prev[n] = u
                 dist[n] = alt
-                hq.heappush(q, (alt, n, ndir))
+                hq.heappush(q, (alt, n))
     return dist, prev
 
 grid = parse_data(example_data,6,6)
 print_grid(grid, 11, 7, 7)
+
+d, p = dijkstra(grid, 7, 7, 11)
